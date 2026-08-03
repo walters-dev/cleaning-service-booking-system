b794ac7c775f1e615f985ea08e6076c90916cc5e
walters-dev <walters.fk10@gmail.com>
2026-08-03 16:41:43 +0200

    Created encryption file

---
+++ CleaningServiceBookingSystemMain/HashCryptography.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+public class Encryption
+{
+	public string EncryptionPassword(string password)
+	{
+		return "";
+    }
+	public string DecryptPassword()
+	{
+		return "";
+	}
+}
