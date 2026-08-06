09173213ebff76397bfd83300d265fea7c2a9841
walters-dev <walters.fk10@gmail.com>
2026-08-06 13:20:40 +0200

    Created menu classes and methods

---
+++ CleaningServiceBookingSystemMain/AdminMenu.cs
@@ -0,0 +1,131 @@
+﻿using Spectre.Console;
+using System;
+
+public class AdminMenu
+{
+	public Class1()
+	{
+
+        //declare and intialize variables
+        bool IsAdminMenuRunning, IsConfirmData;
+        string username, password;
+        Console.WriteLine("Enter Username:");
+        username = Console.ReadLine();
+        //username validation
+
+         Console.WriteLine("Enter Password:");
+         password = Console.ReadLine();
+         //password validation
+         Console.Clear();
+         AnsiConsole.MarkupLine("[green]Signed in[/]");
+         IsAdminMenuRunning = true;
+         while (IsAdminMenuRunning == true)
+         {
+            var adminChoices = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                .Title("Choose menu option")
+                .AddChoices("Create Booking", "Create New Customer", "View Bookings", "View Customers", "Change user"));
+            switch (adminChoices)
+            {
+                case "Create Booking":
+                    AnsiConsole.MarkupLine("[green]Create booking selected[/]");
+                    var createBookingChoices = AnsiConsole.Prompt(
+                        new SelectionPrompt<string>()
+                        .Title("Choose customer:")
+                        .AddChoices("Existing customer", "New customer"));
+
+                    if (createBookingChoices == "Existing customer")
+                    {
+                        AnsiConsole.MarkupLine("[green]Existing customer selected[/]");
+                    }
+                    else if (createBookingChoices == "New customer")
+                    {
+                        IsConfirmData = false;
+                        while (IsConfirmData == false)
+                        {
+                            AnsiConsole.MarkupLine("[green]New Customer selected[/]");
+                            //new customer proccess 
+                            var confirmNewCusChoices = AnsiConsole.Prompt(
+                                new SelectionPrompt<string>()
+                                .Title("Is the customer details correct:")
+                                .AddChoices("Yes", "No"));
+
+                            if (confirmNewCusChoices == "Yes")
+                            {
+                                IsConfirmData = true;
+                                //save customer data to sql
+                            }
+                            else
+                            {
+                                IsConfirmData = false; // loop it
+                            }
+                        }
+                    }
+                     /*
+                     System displays house types and service types from SQL Server
+                     Staff enters number of rooms, booking date, add-ons and recurring option.
+                     System validates all inputs and calculates subtotal, discount, surcharge and final total
+                     */
+                    IsConfirmData = false;
+                    while (IsConfirmData == false)
+                    {
+                        var confirmBookingChoice = AnsiConsole.Prompt(
+                                new SelectionPrompt<string>()
+                                .Title("Is the booking details correct:")
+                                .AddChoices("Yes", "No"));
+
+                        if (confirmBookingChoice == "Yes")
+                        {
+                            IsConfirmData = true;
+                            //save booking data to sql
+                        }
+                        else
+                        {
+                            IsConfirmData = false; // loop it
+                        }
+                    }
+                    break;
+                case "Create New Customer":
+
+                    AnsiConsole.MarkupLine("[green]New Customer selected[/]");
+
+                    IsConfirmData = false;
+                    while (IsConfirmData == false)
+                    {
+                        AnsiConsole.MarkupLine("[green]New Customer selected[/]");
+                        //new customer proccess 
+                        var confirmNewCusChoices = AnsiConsole.Prompt(
+                           new SelectionPrompt<string>()
+                           .Title("Is the customer details correct:")
+                           .AddChoices("Yes", "No"));
+
+                        if (confirmNewCusChoices == "Yes")
+                        {
+                            IsConfirmData = true;
+                            //save customer data to sql
+                        }
+                        else
+                        {
+                            IsConfirmData = false; // loop it
+                        }
+                    }
+                    break;
+                case "View Bookings":
+                    //enter booking date and customer
+                    AnsiConsole.MarkupLine("[green]View Bookings selected[/]");
+                    var viewBookingsChoices = AnsiConsole.Prompt(
+                            new SelectionPrompt<string>()
+                            .Title("Choose option:")
+                            .AddChoices("View", "Report", "Update", "Change status"));
+                    break;
+                case "View Customers":
+                    AnsiConsole.MarkupLine("[green]View Customers selected[/]");
+                    //select customer by contact
+                    break;
+                case "Change user":
+                    IsAdminMenuRunning = false;
+                    break;
+            }
+        }
+        
+}
---
+++ CleaningServiceBookingSystemMain/ManagerMenu.cs
@@ -0,0 +1,32 @@
+﻿using Spectre.Console;
+using System;
+
+public class ManagerMenu
+{
+	public Class1()
+	{
+        //declare and intialize variables
+        bool IsManagerRunning = true;
+        while (IsManagerRunning == true)
+        {
+            var managerChoices = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                .Title("Choose menu option:")
+                .AddChoices("Bookings", "Summaries", "Trends", "Change user"));
+            switch (managerChoices)
+            {
+                case "Bookings":
+                    AnsiConsole.MarkupLine("[green]Booking selected[/]");
+                    break;
+                case "Summaries":
+                    AnsiConsole.MarkupLine("[green]Summaries selected[/]");
+                    break;
+                case "Trends":
+                    AnsiConsole.MarkupLine("[green]Trends selected[/]");
+                    break;
+                case "Change user":
+                    IsManagerRunning = false;
+                    break;
+            }
+        }
+}
