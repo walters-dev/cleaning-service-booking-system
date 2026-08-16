ad851a3dbb64b523336512dc1d6f1ef2ccc2dce8
walters-dev <walters.fk10@gmail.com>
2026-08-16 22:05:14 +0200

    edit interfaces

--- CleaningServiceBookingSystemMain/Application/Interfaces.cs
+++ CleaningServiceBookingSystemMain/Application/Interfaces.cs
@@ -6,10 +6,35 @@ namespace CleaningServiceBookingSystemMain.Application
 {
     public interface ICustomerRepository
     {
-
+        void Add(string Fullname);
+        void Update(string);
     }
     public interface IBookingsRepository 
     {
         
+    }
+    public interface IAdmin
+    {
+
+    }
+    public interface IHouseTypes
+    {
+
+    }
+    public interface IServiceTypes
+    {
+
+    }
+    public interface IDiscountRules
+    {
+
+    }
+    public interface IAddOns
+    {
+
+    }
+    public interface IBookingAddOns
+    {
+
     }
 }
