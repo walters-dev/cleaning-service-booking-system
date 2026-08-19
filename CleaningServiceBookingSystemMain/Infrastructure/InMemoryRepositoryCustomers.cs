29de2c3b7242974d5bb8ff870fe586ed0001219b
walters-dev <walters.fk10@gmail.com>
2026-08-19 15:58:53 +0200

    InMemoryRepo more added

--- CleaningServiceBookingSystemMain/Infrastructure/InMemoryRepositoryCustomers.cs
+++ CleaningServiceBookingSystemMain/Infrastructure/InMemoryRepositoryCustomers.cs
@@ -16,8 +16,7 @@ namespace CleaningServiceBookingSystemMain.Infrastructure
          public IList<Customers> GetCustomers()
         {
             List<Customers> customersInfo = new List<Customers>();
-            using (SqlConnection connection = new SqlConnection(
-               connectionString))
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 SqlCommand command = new SqlCommand("queryString", connection);//waiting for sql procedure
                 command.CommandType = CommandType.StoredProcedure;
@@ -27,26 +26,55 @@ namespace CleaningServiceBookingSystemMain.Infrastructure
                 {
                     var customer = new Customers()
                     {
-                        CustomerId = reader["CustomerId"].ToString(),
-                        FullName = reader["FullName"].ToString(),
-                        PhoneNumber = reader["PhoneNumber"].ToString(),
-                        Email = reader["Email"].ToString(),
-                        PhyAddress = reader["Phy"].ToString(),
+                        CustomerId = reader.GetString(reader.GetOrdinal("CustomerId")),
+                        FullName = reader.GetString(reader.GetOrdinal("FullName")),
+                        PhoneNumber = reader.GetString(reader.GetOrdinal("PhoneNumber")),
+                        Email = reader.GetString(reader.GetOrdinal("Email")),
+                        PhyAddress = reader.GetString(reader.GetOrdinal("PhyAddress")),
                         CreatedAt = reader.GetDateTime(reader.GetOrdinal("CreatedAt"))
                     };
+                    customersInfo.Add(customer);
                 }
-                //command.ExecuteNonQuery();
+                
             }
             return customersInfo;
         }
         public Customers GetCustomerById(int? Id)// ? means it can be null
         {
-            Customers customers = new Customers();
-            return customers;
+            Customers customersInfo = new Customers();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("queryString", connection);//waiting for sql procedure
+                command.CommandType = CommandType.StoredProcedure;
+                command.Connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    customersInfo.CustomerId = reader.GetString(reader.GetOrdinal("CustomerId"));
+                    customersInfo.FullName = reader.GetString(reader.GetOrdinal("FullName"));
+                    customersInfo.PhoneNumber = reader.GetString(reader.GetOrdinal("PhoneNumber"));
+                    customersInfo.Email = reader.GetString(reader.GetOrdinal("Email"));
+                    customersInfo.PhyAddress = reader.GetString(reader.GetOrdinal("PhyAddress"));
+                    customersInfo.CreatedAt = reader.GetDateTime(reader.GetOrdinal("CreatedAt"));
+                }
+            }
+            return customersInfo;
         }
         public void Add(Customers customers)
         {
-
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("queryString", connection);//waiting for sql procedure
+                command.CommandType = CommandType.StoredProcedure;
+                command.Connection.Open();
+                //need to add a thing for id
+                command.Parameters.AddWithValue("@Fullname", customers.FullName);
+                command.Parameters.AddWithValue("@PhoneNumber", customers.PhoneNumber);
+                command.Parameters.AddWithValue("@Email", customers.Email);
+                command.Parameters.AddWithValue("@PhyAddress", customers.PhyAddress);
+                command.Parameters.AddWithValue("@CreatedAt", customers.CreatedAt);
+                command.ExecuteNonQuery();
+            }
         }
         public void Update(Customers customers)
         {
