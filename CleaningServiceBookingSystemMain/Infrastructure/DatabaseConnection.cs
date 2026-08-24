4115765891f411d35047000e71bc765285b73ebc
walters-dev <walters.fk10@gmail.com>
2026-08-24 16:38:38 +0200

    connection str

---
+++ CleaningServiceBookingSystemMain/Infrastructure/DatabaseConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleaningServiceBookingSystemMain.Infrastructure
+{
+    public class DatabaseConnection
+    {
+        public string ConnectionString { get; private set{ ConnectionString= "Server=localhost;Database=CleaningServiceBooking;Trusted_Connection=True;TrustServerCertificate=True;" }; }
+        public 
+
+    }
+}
