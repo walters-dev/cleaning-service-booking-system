988f20b61cef476914761c0ed8ce7c008d58fea6
walters-dev <walters.fk10@gmail.com>
2026-08-25 15:15:53 +0200

    Created db connection

--- CleaningServiceBookingSystemMain/Infrastructure/DatabaseConnection.cs
+++ CleaningServiceBookingSystemMain/Infrastructure/DatabaseConnection.cs
@@ -6,8 +6,11 @@ namespace CleaningServiceBookingSystemMain.Infrastructure
 {
     public class DatabaseConnection
     {
-        public string ConnectionString { get; private set{ ConnectionString= "Server=localhost;Database=CleaningServiceBooking;Trusted_Connection=True;TrustServerCertificate=True;" }; }
-        public 
+        public string ConnectionString { get; private set; }
+        public DatabaseConnection()
+        {
+            ConnectionString = "Server=localhost\\SQLEXPRESS;Database=CleaningServiceBooking;Trusted_Connection=True;TrustServerCertificate=True;";
+        }
 
     }
 }
